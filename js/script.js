1d6e7c9c9daf126020eac136b388bf79f30966cf
Erica-Cradeur <79936012+Erica-Cradeur@users.noreply.github.com>
2023-08-10 15:18:31 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,79 @@
+let isNightMode = false;
+
+function NightDay() {
+  const body = document.body;
+  const calculator = document.querySelector('.calculator');
+  const buttons = document.querySelectorAll('.btn');
+  const colorPaletteButton = document.getElementById('colorPaletteButton');
+  
+  // Example night and day color palette, you can customize these colors
+  const nightBackgroundColor = '#001a0c';
+  const nightDisplayScreen = '#37594a';
+  const nightButtonBackgroundColor = '#4b7563';
+  const nightButtonTextColor = '#cfd1d1';
+  const nightOperatorButtonColor = '#657c5d';
+  const nightEqualButton = '#a0b58a';
+  const nightTopButton = '#7c846d';
+  const nightDisplayText = '#d3d3d1';
+  const nightCalculator = '#37594a';
+
+  const dayBackgroundColor = '#c3c3ae'; 
+  const dayDisplayScreen = '#cfd1d1';
+  const dayButtonBackgroundColor = '#acac93';
+  const dayButtonTextColor = '#12111a';
+  const dayOperatorButtonColor = '#a0b58a';
+  const dayEqualButton = '#37594a';
+  const dayTopButton = '#609998';
+  const dayDisplayText = '#12111a';
+  const dayCalculator = '#cfd1d1';
+
+  isNightMode = !isNightMode;
+
+  body.style.backgroundColor = isNightMode ? nightBackgroundColor : dayBackgroundColor;
+  calculator.style.backgroundColor = isNightMode ? nightBackgroundColor : dayBackgroundColor;
+
+  buttons.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightButtonBackgroundColor : dayButtonBackgroundColor;
+    button.style.color = isNightMode ? nightButtonTextColor : dayButtonTextColor;
+  });
+
+  const operatorButtons = document.querySelectorAll('.btn-operator');
+  operatorButtons.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightOperatorButtonColor : dayOperatorButtonColor;
+  });
+
+  const EqualButton = document.querySelectorAll('.btn-equal');
+  EqualButton.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightEqualButton : dayEqualButton;
+  });
+
+  const TopButton = document.querySelectorAll('.btn-top');
+  TopButton.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightTopButton : dayTopButton;
+  });
+
+  const DisplayText = document.querySelectorAll('#inputDisplay');
+  DisplayText.forEach(button => {
+    button.style.color = isNightMode ? nightDisplayText : dayDisplayText;
+  });
+
+  const DisplayScreen = document.querySelectorAll('.display');
+  DisplayScreen.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightDisplayScreen : dayDisplayScreen;
+  });
+
+  const Calculator = document.querySelectorAll('.calculator');
+  Calculator.forEach(button => {
+    button.style.backgroundColor = isNightMode ? nightCalculator : dayCalculator;
+  });
+
+  if (isNightMode) {
+    colorPaletteButton.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+  } else {
+    colorPaletteButton.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+  }
+}
+
 let currentInput = '0';
 let currentOperator = '';
 let previousInput = '';
@@ -26,12 +102,33 @@ function handleDigitClick(digit) {
 }
 
 function handleOperatorClick(operator) {
-  if (currentOperator !== '') {
-    calculate();
+  if (operator === '%') {
+    calculatePercentage();
+  } 
+  else {
+    if (currentOperator !== '') {
+      calculate();
+    }
+    previousInput = currentInput;
+    currentInput = '0';
+    currentOperator = operator;
   }
-  previousInput = currentInput;
-  currentInput = '0';
-  currentOperator = operator;
+}
+
+function calculatePercentage() {
+  const num = parseFloat(currentInput);
+  const percentage = num / 100;
+
+  currentInput = percentage.toString();
+  shouldClearDisplay = true;
+  updateDisplay();
+}
+
+function toggleSign() {
+  if (currentInput !== '0') {
+    currentInput = (parseFloat(currentInput) * -1).toString();
+    updateDisplay();
+  }
 }
 
 function calculate() {
